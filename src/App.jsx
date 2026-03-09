0334307bf411f68ec070f1ad9694b19bfd0d045e
roy_wang <roy_wang@paytronex.com>
2026-03-09 11:19:37 +0800

    init-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
+import styled from 'styled-components';
+
+// Example for styled component
+const H1 = styled.h1`
+    font-size: 30px;
+    font-weight: 600;
+    color: red;
+`;
+
 function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  )
+    return (
+        <div>
+            <H1>The Wild Oasis</H1>
+        </div>
+    );
 }
 
-export default App
+export default App;
